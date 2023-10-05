60d9b43ee143bb20d45419be144caf6b0e84c50d
megantariaisyah <megantariaisyah@gmail.com>
2023-10-05 09:07:26 -0400

    i change the 2 nav

---
+++ MainScript.js
@@ -0,0 +1,68 @@
+console.log("Hello World! This is a separete JS file!");
+
+
+
+//Exercise 1
+
+
+var x;
+var y;
+
+
+x = 90;
+y = 7;
+
+let z = x + y;
+
+console.log("Addition of " + x + " + " + y + " is " + z)
+
+
+var firstname = "Chiharu";
+var lastname = "Higashi";
+var fullName = firstname + " " + lastname;
+
+console.log("MY NAME IS: " + fullName);
+
+
+if (firstname == "Chiharu") {
+
+}
+
+
+
+let incrementNum = 0; //Testing ++/--
+console.log("the number: " + incrementNum);
+
+incrementNum++;
+incrementNum++;
+incrementNum--;
+
+console.log("the incremented number: " + incrementNum);
+
+//Introducing myself
+
+let sayHelloInJapanese = "こんにちは",  //Hello in Japanse - "こんにちは/Konnichiwa"
+    foodName = "Ramen",
+    drinkName = "coffee",
+    gameName = "splatoon",
+    hobbyName = "going to art museums",
+    countryName = "Japan";
+
+console.log(sayHelloInJapanese + "!" + "My favorite food is " + foodName + ".");
+console.log("My favorite game is " + gameName + ", my hobby is " + hobbyName + ".");
+console.log("I'm from " + countryName + ".");
+
+
+// Making footer copy right:
+
+let d = new Date();
+let year = d.getFullYear();
+let copyright = "All rights reserved ";
+
+console.log(year + " " + fullName);
+
+
+
+
+
+
